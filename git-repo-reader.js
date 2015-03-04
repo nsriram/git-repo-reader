5d736286aebe647b199baf4e3bee5892da331f40
Sriram Narasimhan <nsriram@collab.net>
2015-03-04 20:50:28 +0530

    Sriram: Reading commit hash using pako

--- git-repo-reader.js
+++ git-repo-reader.js
@@ -28,8 +28,7 @@ app.controller('GitRepoReaderCtrl', ['$scope', '$upload', function ($scope, $upl
       var fileReader = new FileReader();
       fileReader.onload = function (onLoadEvent) {
         var content = onLoadEvent.target.result;
-        var contentLines = content.split('\n');
-        var refHead = contentLines[0];
+        var refHead = content.replace(/^\s+|\s+$/g, '');
         var masterIndex = refHead.indexOf(REF);
         if (masterIndex > -1) {
           var master = refHead.substring(masterIndex + REF.length);
@@ -49,15 +48,41 @@ app.controller('GitRepoReaderCtrl', ['$scope', '$upload', function ($scope, $upl
       var fileReader = new FileReader();
       fileReader.onload = function (onLoadEvent) {
         $scope.$apply(function () {
-          $scope.headCommitHash = onLoadEvent.target.result;
+          var ref = onLoadEvent.target.result;
+          $scope.headCommitHash = ref.replace(/^\s+|\s+$/g, '');
         });
       };
       fileReader.readAsText($scope.headFileRef);
     }
   });
 
-  function filter(filterCriteria) {
+  $scope.$watch('headCommitHash', function (newValue, oldValue) {
+    if (newValue !== oldValue) {
+      var objectDir = $scope.headCommitHash.substr(0, 2);
+      var objectFile = $scope.headCommitHash.substr(2);
+      var headObjectPath = $scope.baseDirectory + '.git/objects/' + objectDir + '/' + objectFile;
+      var headObjectFile = filter(headObjectPath);
+      var fileReader = new FileReader();
+      fileReader.onloadend = function () {
+        var inflator = new pako.Inflate();
+        inflator.push(fileReader.result, true);
+        if (inflator.err) {
+          console.log(inflator.msg);
+        }
+        var output = inflator.result;
+        console.log(output);
+        var str = String.fromCharCode.apply(null, output);
+        console.log(str);
+      };
+      fileReader.readAsBinaryString(headObjectFile[0]);
+    }
+  });
+
+  function filter(filterCriteria, print) {
     return $scope.files.filter(function (dirFile) {
+      if (print) {
+        console.log(dirFile.webkitRelativePath + ' , ' + filterCriteria);
+      }
       return dirFile.webkitRelativePath.indexOf(filterCriteria) > -1;
     });
   }
